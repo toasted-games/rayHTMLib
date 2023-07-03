afb2d63298b74989656b82dc531c799b60c99bfc
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-03 16:56:16 +0200

    :sparkles: Read file content to string

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -7,9 +7,27 @@ Core::Core()
 int Core::loadLayout(std::string layout)
 {
     std::cout << "Loading layout: " << layout << std::endl;
+
+    std::string content = readFile(layout);
+
+    std::cout << "Layout content: " << content << std::endl;
     return 0;
 }
 
 std::string Core::readFile(std::string filename)
 {
+    std::string content = "";
+
+    std::fstream file(filename, std::ios::in);
+
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        content += line;
+    }
+
+    file.close();
+
+    return content;
 }
