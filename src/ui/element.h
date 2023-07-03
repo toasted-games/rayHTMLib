7cf5c504ccf5d2c5ed8dbb694e92f3ca620c346d
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-03 19:21:48 +0200

    :sparkles: Add `ElementType` enum

--- src/ui/element.h
+++ src/ui/element.h
@@ -1,6 +1,33 @@
 #ifndef RAYHTMLIB_UI_ELEMENT_H
 #define RAYHTMLIB_UI_ELEMENT_H
 
+enum ElementType
+{
+    MAIN,
+    DIV,
+    SPAN,
+    BUTTON,
+    INPUT,
+    LABEL,
+    TEXTAREA,
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    P,
+    TABLE,
+    TR,
+    TD,
+    TH,
+    IMG,
+    A,
+    UL,
+    OL,
+    LI,
+};
+
 class Element
 {
 public:
