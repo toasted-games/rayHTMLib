bc7f3f58558aa56251d5a7c4c146822575a685ce
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-03 20:09:20 +0200

    :sparkles: Add regex to `core.h`

--- src/core/core.h
+++ src/core/core.h
@@ -16,6 +16,12 @@ public:
     int loadLayout(std::string layout);
 
 private:
+    std::string elementRegex = "<[^\/](\X[^>]+)>";
+    std::string tagRegex = "<([a-zA-Z]+).{0,}>";
+    std::string tagCloseRegex = "<\/([a-zA-Z]+)>";
+    std::string idRegex = "id=\"([a-zA-Z0-9]+)\"";
+    std::string classRegex = "class=\"([a-zA-Z0-9 ]+)\"";
+
     std::string readFile(std::string filename);
 };
 
